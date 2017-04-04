4261c76ff0b0412f997abe6e3de21eae3d9958e7
Jesse <scalpel.etf@gmail.com>
2017-04-04 09:42:08 -0500

    openMP finished

--- Computer_Arc_Sim/Multi_Core/fft.c
+++ Computer_Arc_Sim/Multi_Core/fft.c
@@ -25,6 +25,23 @@ void fft(int N, float (*x)[2], float (*X)[2])
 void fft_rec(int N, int offset, int delta,
              float (*x)[2], float (*X)[2], float (*XX)[2])
 {
+  omp_set_num_threads(N_PROC);
+
+//   #pragma omp parallel sections
+// {
+//     #pragma omp section
+//     { 
+//         printf("FFT_REC \n");
+//         printf ("id = %d, \n", omp_get_thread_num());
+//     }
+
+//     #pragma omp section
+//     { 
+//         printf("FFT_REC \n");
+//         printf ("id = %d, \n", omp_get_thread_num());
+//     }
+// }
+
   int N2 = N/2;            /* half the number of points in FFT */
   int k;                   /* generic index */
   float cs, sn;           /* cosine and sine */
@@ -38,19 +55,77 @@ void fft_rec(int N, int offset, int delta,
       fft_rec(N2, offset+delta, 2*delta, x, XX, X);
 
       /* Combine the two (N/2)-point DFT's into one N-point DFT. */
-      for(k=0; k<N2; k++)
+      #pragma omp parallel sections
+      {
+        #pragma omp section
         {
-          k00 = offset + k*delta;    k01 = k00 + N2*delta;
-          k10 = offset + 2*k*delta;  k11 = k10 + delta;
-          cs = cos(TWO_PI*k/(float)N); sn = sin(TWO_PI*k/(float)N);
-          tmp0 = cs * XX[k11][0] + sn * XX[k11][1];
-          tmp1 = cs * XX[k11][1] - sn * XX[k11][0];
-          X[k01][0] = XX[k10][0] - tmp0;
-          X[k01][1] = XX[k10][1] - tmp1;
-          X[k00][0] = XX[k10][0] + tmp0;
-          X[k00][1] = XX[k10][1] + tmp1;
+          for(k=0; k<N2/4; k++)
+            {
+              k00 = offset + k*delta;    k01 = k00 + N2*delta;
+              k10 = offset + 2*k*delta;  k11 = k10 + delta;
+              cs = cos(TWO_PI*k/(float)N); sn = sin(TWO_PI*k/(float)N);
+              tmp0 = cs * XX[k11][0] + sn * XX[k11][1];
+              tmp1 = cs * XX[k11][1] - sn * XX[k11][0];
+              X[k01][0] = XX[k10][0] - tmp0;
+              X[k01][1] = XX[k10][1] - tmp1;
+              X[k00][0] = XX[k10][0] + tmp0;
+              X[k00][1] = XX[k10][1] + tmp1;
+            }
         }
-    }
+
+        #pragma omp section
+        {
+          for(k=N2/4; k<N2/2; k++)
+            {
+              k00 = offset + k*delta;    k01 = k00 + N2*delta;
+              k10 = offset + 2*k*delta;  k11 = k10 + delta;
+              cs = cos(TWO_PI*k/(float)N); sn = sin(TWO_PI*k/(float)N);
+              tmp0 = cs * XX[k11][0] + sn * XX[k11][1];
+              tmp1 = cs * XX[k11][1] - sn * XX[k11][0];
+              X[k01][0] = XX[k10][0] - tmp0;
+              X[k01][1] = XX[k10][1] - tmp1;
+              X[k00][0] = XX[k10][0] + tmp0;
+              X[k00][1] = XX[k10][1] + tmp1;
+            }
+        }
+
+        #pragma omp section
+        {
+          for(k=N2/2; k<3*N2/4; k++)
+            {
+              k00 = offset + k*delta;    k01 = k00 + N2*delta;
+              k10 = offset + 2*k*delta;  k11 = k10 + delta;
+              cs = cos(TWO_PI*k/(float)N); sn = sin(TWO_PI*k/(float)N);
+              tmp0 = cs * XX[k11][0] + sn * XX[k11][1];
+              tmp1 = cs * XX[k11][1] - sn * XX[k11][0];
+              X[k01][0] = XX[k10][0] - tmp0;
+              X[k01][1] = XX[k10][1] - tmp1;
+              X[k00][0] = XX[k10][0] + tmp0;
+              X[k00][1] = XX[k10][1] + tmp1;
+            }
+        }
+
+
+        #pragma omp section
+        {
+          for(k=3*N2/4; k<N2; k++)
+            {
+              k00 = offset + k*delta;    k01 = k00 + N2*delta;
+              k10 = offset + 2*k*delta;  k11 = k10 + delta;
+              cs = cos(TWO_PI*k/(float)N); sn = sin(TWO_PI*k/(float)N);
+              tmp0 = cs * XX[k11][0] + sn * XX[k11][1];
+              tmp1 = cs * XX[k11][1] - sn * XX[k11][0];
+              X[k01][0] = XX[k10][0] - tmp0;
+              X[k01][1] = XX[k10][1] - tmp1;
+              X[k00][0] = XX[k10][0] + tmp0;
+              X[k00][1] = XX[k10][1] + tmp1;
+            }
+        }
+
+      }
+  }
+
+
   else  /* Perform 2-point DFT. */
     {
       k00 = offset; k01 = k00 + delta;
@@ -67,18 +142,18 @@ void ifft(int N, float (*x)[2], float (*X)[2])
 
   omp_set_num_threads(N_PROC);
 
-  #pragma omp parallel sections
-{
-    #pragma omp section
-    { 
-        printf ("id = %d, \n", omp_get_thread_num());
-    }
+//   #pragma omp parallel sections
+// {
+//     #pragma omp section
+//     { 
+//         printf ("id = %d, \n", omp_get_thread_num());
+//     }
 
-    #pragma omp section
-    { 
-        printf ("id = %d, \n", omp_get_thread_num());
-    }
-}
+//     #pragma omp section
+//     { 
+//         printf ("id = %d, \n", omp_get_thread_num());
+//     }
+// }
 
   int N2 = N/2;       /* half the number of points in IFFT */
   int i;              /* generic index */
